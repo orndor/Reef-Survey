2edfea334a241cffa872d71bd96f7348a604a1e7
Rob Orndoff <rorndoff@gmail.com>
2019-08-22 09:18:07 -0400

    More EF Work

--- Reef.Model/EF-Creation.cs
+++ Reef.Model/EF-Creation.cs
@@ -27,6 +27,8 @@ namespace Reef.Model
         public double Latitude { get; set; }
         public string Management { get; set; }
 
+        public List<Survey> Surveys { get; set; }
+
     }
 
     public class Fish
@@ -36,6 +38,9 @@ namespace Reef.Model
         public string ScientificName { get; set; }
         public string CommonName { get; set; }
         public string Trophic { get; set; }
+
+        public List<Schools> School { get; set; }
+        public List<Survey> Surveys { get; set; }
 
         public int SurveyId { get; set; }
         public Survey Survey { get; set; }
@@ -52,6 +57,8 @@ namespace Reef.Model
         public double FishLength { get; set; }
         public double FishCount { get; set; }
 
+        public List<Fish> Fishs { get; set; }
+
         public int FishId { get; set; }
         public Fish Fish { get; set; }
 
@@ -65,6 +72,8 @@ namespace Reef.Model
         public int BatchCode { get; set; }
         public int SurveyIndex { get; set; }
         public int FishCount { get; set; }
+
+        public List<Fish> Fishs { get; set; }
 
         public int LocationId { get; set; }
         public Location Location { get; set; }
